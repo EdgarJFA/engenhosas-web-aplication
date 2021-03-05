97b2b689b2a0ebf19f3e681dc93ad573c022983f
Edgar Amado <62776977+EdgarJFA@users.noreply.github.com>
2021-03-05 13:43:28 +0200

    feat: Create Pages content

--- src/Pages/Home/Index.tsx
+++ src/Pages/Home/Index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logoImg from '../../assets/icons/solea.png';
 
 import './styles.css';
 
@@ -7,6 +8,7 @@ function Home () {
         <div id="container">
             <div className="heart"></div>
             <div className="text">
+                <img className="logo" src={logoImg} alt="logoEngenhosas"/>
                 <h3>O Sitio web das Engenhosas está em desenvolvimento... Obrigado por esperar</h3>
                 <h3>Voltamos em breve</h3>
             </div>
